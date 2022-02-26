24ffe6326194f7a791ce6c8597e146581266f579
Philip Gyllhamn <philipgyllhamn@hotmail.com>
2022-02-26 18:37:04 +0100

    init

---
+++ test/spec.js
@@ -0,0 +1,10 @@
+const transportAPI = require('../src/index');
+
+
+
+(async () => {
+    const res = await transportAPI.GetVehicleInformation("OBX182");
+    //const res = await transportAPI.GetVehicleInformation("HEJKOLLLLLG");
+    //const res = await transportAPI.TestCrawlV2("MTR00B");
+    console.log(res);
+})();
