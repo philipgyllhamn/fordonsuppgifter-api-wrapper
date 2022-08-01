4d2a85b487e89afb3a1a482cda8fb9beb1773299
Max Malm <benjick@dumfan.net>
2022-08-01 22:19:15 +0200

    no unnecessary waiting

--- test/spec.js
+++ test/spec.js
@@ -1,7 +1,5 @@
 const transportAPI = require('../src/index');
 
-
-
 (async () => {
     const res = await transportAPI.GetVehicleInformation("OBX182");
     //const res = await transportAPI.GetVehicleInformation("HEJKOLLLLLG");
